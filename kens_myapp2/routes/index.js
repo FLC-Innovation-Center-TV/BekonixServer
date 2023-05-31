fa70972bd3dc3412904e93e86eec3ee27aa6ece6
MindMania <108013388+MindMania@users.noreply.github.com>
2023-05-31 16:05:57 -0700

    changed file name logic
    made each part of the date separate variables
    added comments to most lines of code
    got rid of object that holds image sessions, instead uses a global string

--- kens_myapp2/routes/index.js
+++ kens_myapp2/routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 const fs = require('fs');
 
-let imageCounter = 0;  // global counter for the image
-let imageSessions = {};  // object to hold image session data
+let imgNumber = 1; // number of image (grab from database)
+let imgName = ""; // stores the final name of the image file
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -17,40 +17,57 @@ function callback(err){
 
 router.post('/post_page.html', function(req, res, next) {
   var BlockInfo=req.headers['x-camblock'];
+  console.log("BlockInfo: "+BlockInfo+" Body Size:"+req.body.length);
 
   if (req.body.length && BlockInfo){
     var args=BlockInfo.split(",");
 
     var imageId = args[2];  // unique identifier for the image
 
-    // If this is the first block of this image, create a new session
+    // If this is the first block of the image, set the name of the image
     if (args[0] == 1) {
-      imageCounter++;
+      
+      console.log("First Block Received");
+      let date = new Date(); // get current date
 
-      // Get current date and time
-      let date = new Date();
+      // get each value of the date
+      let year = date.getFullYear();
+      let month = date.getMonth() + 1; // add 1 since month starts at 0
+      let day = date.getDate();
+      let hour = date.getHours();
+      let minute = date.getMinutes();
 
-      // Format date and time
-      let formattedDate = ("0" + date.getDate()).slice(-2) + ("0" + (date.getMonth() + 1)).slice(-2) + date.getFullYear().toString().slice(-2);
-      let formattedTime = ("0" + date.getHours()).slice(-2) + ("0" + date.getMinutes()).slice(-2);
+      // get 2 digit string for each value, add "0" to front if value is single digit
+      let yearString = ("" + year).slice(-4);
+      let monthString = ("0" + month).slice(-2);
+      let dayString = ("0" + day).slice(-2);
+      let hourString = ("0" + hour).slice(-2);
+      let minuteString = ("0" + minute).slice(-2);
 
-      // Format image counter
-      let formattedCounter = ("000000" + imageCounter).slice(-6);
+      // get the 6 digit string number, add "0" to front if less than 6 digits
+      let imgNumberString = ("00000" + imgNumber).slice(-6);
 
-      imageSessions[imageId] = `img_${formattedCounter}_${formattedDate}_${formattedTime}.jpeg`;
+      // concatenate all the strings together to get the image name
+      imgName = "img_" + imgNumberString + "_" + yearString + "_" + monthString + "-" + dayString + "_" + hourString + minuteString + ".jpeg";
+      console.log("imgName", imgName);
     }
 
-    var path=process.cwd() + "/output/" + imageSessions[imageId];
+    // set the file path the image gets written to
+    var path=process.cwd() + "/output/" + imgName;
 
+    // first block, overwrite the file from scratch
     if (args[0]==1){
       fs.writeFile(path, req.body, callback);
     }
+    // blocks that come after the first, add the data to the end of the file
     else {
       fs.appendFile(path, req.body, callback);
     }
+    // this is the final block, because the block number equals the total blocks
     if (args[0]==args[1]){
+        // transfer the image to the database with the name we gave it
         console.log("Transfer Complete");
-        delete imageSessions[imageId];  // cleanup session
+        imgNumber++; // update the image number (in the database)
     }
   }
   else {
