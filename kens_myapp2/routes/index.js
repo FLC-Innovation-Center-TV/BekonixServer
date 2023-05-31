3e0437cb1ceb4fdbf4ff732c9c1698ae3299b36c
Colin Casey <colincas37@gmail.com>
2023-05-30 17:48:11 -0700

    change index.js to format files in this formate {img_000000_DayMonthYear_Militarytime}

--- kens_myapp2/routes/index.js
+++ kens_myapp2/routes/index.js
@@ -3,27 +3,44 @@ var router = express.Router();
 
 const fs = require('fs');
 
+let imageCounter = 0;  // global counter for the image
+let imageSessions = {};  // object to hold image session data
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Bekonix Test Page' });
 });
 
-
 function callback(err){
-  console.log(err);
+  if (err) console.log(err);
 }
 
 router.post('/post_page.html', function(req, res, next) {
-  //res.render('index', { title: 'Something New' });
-
-  //console.log("Body size; "+req.body.length);
   var BlockInfo=req.headers['x-camblock'];
-  console.log("BlockInfo: "+BlockInfo+" Body Size:"+req.body.length);
 
   if (req.body.length && BlockInfo){
     var args=BlockInfo.split(",");
 
-    var path=process.cwd()+"/output/out.jpeg";
+    var imageId = args[2];  // unique identifier for the image
+
+    // If this is the first block of this image, create a new session
+    if (args[0] == 1) {
+      imageCounter++;
+
+      // Get current date and time
+      let date = new Date();
+
+      // Format date and time
+      let formattedDate = ("0" + date.getDate()).slice(-2) + ("0" + (date.getMonth() + 1)).slice(-2) + date.getFullYear().toString().slice(-2);
+      let formattedTime = ("0" + date.getHours()).slice(-2) + ("0" + date.getMinutes()).slice(-2);
+
+      // Format image counter
+      let formattedCounter = ("000000" + imageCounter).slice(-6);
+
+      imageSessions[imageId] = `img_${formattedCounter}_${formattedDate}_${formattedTime}.jpeg`;
+    }
+
+    var path=process.cwd() + "/output/" + imageSessions[imageId];
 
     if (args[0]==1){
       fs.writeFile(path, req.body, callback);
@@ -33,31 +50,21 @@ router.post('/post_page.html', function(req, res, next) {
     }
     if (args[0]==args[1]){
         console.log("Transfer Complete");
+        delete imageSessions[imageId];  // cleanup session
     }
-
   }
   else {
     res.status(204).send();
     return;
   }
 
-
   res.end('It worked!');
   return;
-
-
-//  res.statusMessage="TESTTEST12345";
-//  return res.sendStatus(204);
-
-  //204 No Content, the client doesn't need to navigate away from its current page
   res.status(204).send();
 });
 
-
-
-router.get('/Output/out.jpeg', function(req, res, next) {
-
-  var path=process.cwd()+"/output/out.jpeg";
+router.get('/Output/:imageFile', function(req, res, next) {
+  var path=process.cwd()+"/output/"+req.params.imageFile;
   res.sendFile(path);
 });
 
